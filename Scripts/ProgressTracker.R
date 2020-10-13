4dfe51fe95d48e4b9b45c4f4e1a717bb97a5576e
MinistryPoobah <keith.story@gov.bc.ca>
2020-10-13 11:38:14 -0700

    Update paths

--- Scripts/ProgressTracker.R
+++ Scripts/ProgressTracker.R
@@ -9,55 +9,25 @@ library(readxl)
 library(googledrive)
 library(lubridate)
 library(gtools)
-
+ 
 drive_auth(
   email = "epdcompliancedashboard@gmail.com",
 )
 
 `%notin%` <- Negate(`%in%`)
 
-path.set = "C:/Users/kstory/Documents/GrandPoobah_R/Dashboard Data/DashboardDataOutput/"
-setwd("C:/Users/kstory/Documents/GrandPoobah_R/Dashboard Data/DashboardDataOutput/")
+path.set = "//Sfp.idir.bcgov/s140/s40086/WANSHARE/ROB/ARCS/ROB ARCS/Information Technology 6000-6999/6820-20 ArcGIS Dashboard/DashboardDataOutput/ProgressTracker"
+setwd("//Sfp.idir.bcgov/s140/s40086/WANSHARE/ROB/ARCS/ROB ARCS/Information Technology 6000-6999/6820-20 ArcGIS Dashboard/DashboardDataOutput/ProgressTracker") 
+name_key <- read_csv(file = "//Sfp.idir.bcgov/s140/s40086/WANSHARE/ROB/ARCS/ROB ARCS/Information Technology 6000-6999/6820-20 ArcGIS Dashboard/Name Key.csv")
 
-name_key <- read_csv(file = "C:/Users/kstory/Documents/GrandPoobah_R/Dashboard Data/Name Key.csv")
-
-# file.names <- dir(path.set, pattern =".csv")
-# master.file <- list()
-# 
-# extract_summarize <- function(file.names) {
-#   in.file <- read_csv(file = file.names, na = c("", NA,"#N/A"))
-#   file.date <- str_sub(file.names, start = 1, end = 10)
-#   
-#   in.file$`Assigned`[is.na(in.file$`Assigned`)] <- "Unassigned"
-#   in.file$`Assigned`[which(in.file$`Assigned` == "jeffery")] <- "Jeffery"
-#   in.file$`Assigned`[which(in.file$`Assigned` == "White")] <- "T. White"
-#   in.file$`Assigned`[which(in.file$`Assigned` == "Naseri")] <- "Nazeri"
-#   in.file$`Assigned`[which(in.file$`Assigned` == "beck")] <- "Beckett"
-#   
-#   in.file <- in.file %>%
-#     select(Assigned, `Inspected This Fiscal?`) %>%
-#     filter(Assigned %in% unique(name_key$`poobah name`), Assigned != "Unassigned") %>%
-#     filter(`Inspected This Fiscal?` == "Yes") 
-#   
-#   in.file <- as.tibble(with(in.file, table(Assigned, `Inspected This Fiscal?`)))
-#   
-#   in.file <- in.file %>%
-#     select(-`Inspected This Fiscal?`) %>%
-#     rename(!!file.date := n)
-#   
-#   bind_cols
-# }
-# 
-# master.list <- lapply(file.names, extract_summarize)
 #____________________________________________________________
 
 multmerge = function(mypath){
   
-  filenames = list.files(path = mypath, full.names = TRUE)
+  filenames = list.files(path = mypath, pattern = "*.csv", full.names = TRUE)
   
   datalist = lapply(filenames, function(x){
     in.file <- read_csv(file = x)
-    # csv.names <- dir(path = x, pattern =".csv")
     file.date <- str_sub(x, start = -28, end = -19)
     
     in.file$`Assigned`[is.na(in.file$`Assigned`)] <- "Unassigned"
@@ -82,35 +52,28 @@ multmerge = function(mypath){
     in.file <- in.file %>%
       select(-Var2) %>%
       mutate(Date = file.date)
-     #rename(!!file.date := Freq) #%>%
-      # rename(Assigned = Var1)
-    
-    # print(in.file)
     
     })
-  
-  # print(datalist)
-  # Reduce(function(dtf1, dtf2) merge(dtf1, dtf2, by = "Var1", all.x = TRUE), mymergeddata)
-  
 }
 
-mymergeddata = multmerge("C:/Users/kstory/Documents/GrandPoobah_R/Dashboard Data/DashboardDataOutput")
+mymergeddata = multmerge("//Sfp.idir.bcgov/s140/s40086/WANSHARE/ROB/ARCS/ROB ARCS/Information Technology 6000-6999/6820-20 ArcGIS Dashboard/DashboardDataOutput/ProgressTracker")
 
 mymergeddata <- mymergeddata %>%
   bind_rows %>%
-  rename(Assigned = Var1) %>%
-  filter(as.Date(Date) > "2020-04-14")
+  rename(Assigned = Var1)
 
 
 spread_compliance_data <- mymergeddata %>%
   bind_rows %>%
   # mutate(ID = row_number()) %>%
-  spread(Date, Freq) %>%
-  rename(Assigned = Var1)
+  spread(Date, Freq) 
 
 
+mymergeddata_planned <- mymergeddata %>%
+  filter(Assigned %in% c("C. White", "Coleman", "Connolly", "Fraleigh", "Jeffery", "Kurinka", "Manders", "Nazeri", "Pidskalney", "Robinson", "Stewart", "Story", "Strauss", "T.White")) %>%
+  filter(Date > as.Date("2020-05-04"))
 #__________________________________________________________
-(p <- ggplot(data = mymergeddata, aes(as.Date(Date), Freq), color = Assigned) +
+(p <- ggplot(data = mymergeddata_planned, aes(as.Date(Date), Freq), color = Assigned) +
   geom_point() +
   geom_smooth(method = "loess") +
   facet_wrap(. ~ Assigned) +
@@ -118,16 +81,14 @@ spread_compliance_data <- mymergeddata %>%
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
   )
 
-# ___________________________________________________________________
-
-(ComplianceTeamOverview <- ggplot(data = mymergeddata, aes(as.Date(Date), Freq), color = Assigned) +
+ComplianceTeamOverview <- ggplot(data = mymergeddata_planned, aes(as.Date(Date), Freq), color = Assigned) +
     geom_point(color = "white") +
     geom_smooth(method = "loess", color = "olivedrab2") +
     theme_light() +
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
     xlab("Month (2020)") +
     ylab("# of Inspections, Cumulative") +
-    ggtitle("Compliance Team Overview") +
+    ggtitle("Compliance Planned Team Overview") +
     theme_dark() +
     
     theme(plot.background = element_rect(fill = "grey17"),
@@ -136,13 +97,12 @@ spread_compliance_data <- mymergeddata %>%
           axis.title.x = element_text(color = "white", size = 15),
           axis.title.y = element_text(color = "white", size = 15)
           )
-)
 
-png("C:/Users/kstory/Documents/GrandPoobah_R/Dashboard Data/ComplianceTeamOverview.png")
+png("//Sfp.idir.bcgov/s140/s40086/WANSHARE/ROB/ARCS/ROB ARCS/Information Technology 6000-6999/6820-20 ArcGIS Dashboard/DashboardDataOutput/ComplianceTeamOverview.png")
 print(ComplianceTeamOverview)
 dev.off()
 
 # (Plots <- drive_upload(media = "C:/Users/kstory/Documents/GrandPoobah_R/Dashboard Data/ComplianceTeamOverview.png", path = "Plots/", overwrite = TRUE))
-(Plots <- drive_update(file = as_id("1B_cz-RFU9-0EzwFtMIg7Lox3J3QLK9Nb"), media = "C:/Users/kstory/Documents/GrandPoobah_R/Dashboard Data/ComplianceTeamOverview.png"))
+(Plots <- drive_update(file = as_id("1B_cz-RFU9-0EzwFtMIg7Lox3J3QLK9Nb"), media = "//Sfp.idir.bcgov/s140/s40086/WANSHARE/ROB/ARCS/ROB ARCS/Information Technology 6000-6999/6820-20 ArcGIS Dashboard/DashboardDataOutput/ComplianceTeamOverview.png"))
 
 
